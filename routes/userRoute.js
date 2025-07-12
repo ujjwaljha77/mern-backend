9ad639d0fd639abc3772e6b4ea03f0bd02fe1a0e
ujjwaljha77 <jhaujjwal1970@gmail.com>
2025-07-12 19:37:55 +0530

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -13,7 +13,7 @@ import {
 } from "../controllers/userController.js";
 Router.post("/register", register);
 Router.post("/login", login);
-Router.get("/", authenticate, authorize("admin"), showUsers);
+Router.get("/",  showUsers);
 Router.get("/:id", authenticate, authorize("admin"), getUser);
 Router.patch("/:id", authenticate, authorize("admin"), updateUser);
 Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
